ca4c5c0b0a23b5f94c4336cf505b3ec41bd863e4
Chiwai Yau <chiwai.yau@gravitysupplychain.com>
2021-12-30 10:12:50 +0800

    added comment

--- fob/index.js
+++ fob/index.js
@@ -5,3 +5,17 @@ console.log(advance.fob(100));
 console.timeEnd('50')
 
 console.timeEnd('full')
+
+// if the current pile has at most M boxes, you can carry it as is
+// otherwise you split it into P parts as equally as possible 
+// (i.e. so that the sizes of the parts differ at most by 1) and apply the same algorithm to each of the parts.
+// algorithm to split pile into parts
+// if pile is empty, return empty array
+// if pile has 1 box, return pile
+// if pile has more than 1 box, split pile into parts
+// if pile has more than 1 box and more than M boxes, split pile into parts
+function fob(M, P) {
+  if (M === 0) {
+    return 0;
+  }
+}
